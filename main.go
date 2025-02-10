b26db3d5dffae58f6c948126faefd625a3e05b25
nilabh <nilabh.subramaniam@amasol.in>
2025-02-10 17:35:26 +0530

    initial commit for mod init and fmt package import

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var y int
+
+func main() {
+	// printing value for hexadecimal and binary
+	// a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
+	// fmt.Printf("%v  \t  %b \t %#X \n", a, a, a)
+	// fmt.Printf("%v  \t  %b \t %#X \n", b, b, b)
+	// fmt.Printf("%v  \t  %b \t %#X \n", c, c, c)
+	// fmt.Printf("%v  \t  %b \t %#X \n", d, d, d)
+	// fmt.Printf("%v  \t  %b \t %#X \n", f, f, f)
+	// fmt.Printf("%v  \t  %b \t %#X \n", e, e, e)
+	// fmt.Printf("%v  \t  %b \t %#X \n", d, d, d)
+	fmt.Println(y)
+	z := 42
+	fmt.Println(z)
+	fmt.Println("Hello, World!")
+}
