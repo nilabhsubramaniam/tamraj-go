4920226928c02590a1ff784818aede63df629047
nilabh <nilabh.subramaniam@amasol.in>
2025-02-13 15:49:43 +0530

    select switch and import of inbuilt functions

--- main.go
+++ main.go
@@ -1,24 +1,56 @@
 package main
 
-import "fmt"
-
-var x int = 40
-
-const y int = 42
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	// printing value for hexadecimal and binary
-	// a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
-	// fmt.Printf("%v  \t  %b \t %#X \n", a, a, a)
-	// fmt.Printf("%v  \t  %b \t %#X \n", b, b, b)
-	// fmt.Printf("%v  \t  %b \t %#X \n", c, c, c)
-	// fmt.Printf("%v  \t  %b \t %#X \n", d, d, d)
-	// fmt.Printf("%v  \t  %b \t %#X \n", f, f, f)
-	// fmt.Printf("%v  \t  %b \t %#X \n", e, e, e)
-	// fmt.Printf("%v  \t  %b \t %#X \n", d, d, d)
-	fmt.Printf("The value of x %v and the type of x %T \n", x, x)
-	fmt.Printf("The value of y %v and the type of y %T \n", y, y)
-	z := 42
-	fmt.Println(z)
-	fmt.Println("Hello, World!")
+	src := rand.NewSource(time.Now().UnixNano()) // Create a new source
+	rng := rand.New(src)                         // Create a new random generator
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	x := 10 // Define x
+	y := 5  // Define y
+
+	if z := 2 * rng.Intn(x); z >= x {
+		fmt.Printf("The value of x %v and the type of x %T \n", x, x)
+	} else {
+		fmt.Printf("The value of z %v and the type of y %T \n", z, y) // Corrected: use z, not y
+	}
+
+	day := "Monday"
+
+	switch day {
+	case "Monday":
+		fmt.Println("Start of the workweek!")
+	case "Friday":
+		fmt.Println("Weekend is near!")
+	default:
+		fmt.Println("It's a regular day.")
+	}
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch1 <- "Message from Channel 1 "
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch2 <- "Message from Channel 2 "
+	}()
+
+	// select statement to understand
+
+	select {
+	case msg1 := <-ch1:
+		fmt.Println(msg1)
+	case ms2 := <-ch2:
+		fmt.Println(ms2)
+	default:
+		fmt.Println("No message received, moving on...")
+
+	}
 }
