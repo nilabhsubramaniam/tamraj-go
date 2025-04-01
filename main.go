7f3834732ff455a9d24b5649fb9f62ee7552744a
nilabh <nilabh.subramaniam@amasol.in>
2025-04-01 09:55:10 +0530

    use for loop

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	src := rand.NewSource(time.Now().UnixNano()) // Create a new source
 	rng := rand.New(src)                         // Create a new random generator
+	ch := make(chan string)
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
@@ -49,8 +50,17 @@ func main() {
 		fmt.Println(msg1)
 	case ms2 := <-ch2:
 		fmt.Println(ms2)
+	case <-time.After(2 * time.Second):
+		fmt.Println("Timeout! No response within 2 seconds.")
+	case msg := <-ch:
+		fmt.Println("Received:", msg)
 	default:
 		fmt.Println("No message received, moving on...")
 
+	}
+
+	// for loop
+	for i := 0; i < 5; i++ {
+		fmt.Printf("counting to five: %v \t first for loop \n ", i)
 	}
 }
